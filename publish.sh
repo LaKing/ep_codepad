8ac2c5380ba0671f669b75faca4c6c0f2fd2dfaa
codepad <codepad@codepad.d250.hu>
2014-09-20 15:23:47 +0200

    0.8.6

--- publish.sh
+++ publish.sh
@@ -1,4 +1,25 @@
-{
+#!/bin/bash 
+
+# Last update:2014.09.20-16:00:00 
+# 
+# DEV ep_codepad publishing script. 
+# 
+# D250 Laboratories / D250.hu 
+# Author: István király 
+# LaKing@D250.hu 
+# 
+
+g='package.json'
+
+
+NOW=$(date +%Y.%m.%d-%H:%M:%S) 
+echo $NOW  
+ 
+cv=`cat $g | grep "version" | sed 's/[^0-9.]*//g'` 
+nv=`echo $cv | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'` 
+echo $cv" > "$nv
+
+echo '{
     "name": "ep_codepad",
     "description": "Turn etherpad-lite into a realtime collaborative development environment",
     "url": "https://github.com/LaKing/ep_codepad",
@@ -21,7 +42,7 @@
     "email": "LaKing@D250.hu",
     "homepage": "http://www.D250.hu"
     },
-    "version": "0.8.6",
+    "version": "'$nv'",
     "dependencies": {
         "js-beautify": ">=1.5.0",
         "jshint": ">= 2.5.0",
@@ -38,4 +59,12 @@
     "dist": {
         "shasum": "a8f8e9afb5f219265ffb696ca5cf4cf9c1d778c8"
     },
-}
+}' > $g
+            
+git add .
+git commit -m $nv
+git push
+
+npm publish
+
+
